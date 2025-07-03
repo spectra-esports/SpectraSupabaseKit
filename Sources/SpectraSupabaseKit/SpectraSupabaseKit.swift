a7adb0a3e99d19e4fd3770d6c4701b35f7f6cb0b
vegopunk <vegopunk@mail.ru>
2025-07-03 17:32:46 +0200

    Update SpectraSupabaseKit.swift

--- Sources/SpectraSupabaseKit/SpectraSupabaseKit.swift
+++ Sources/SpectraSupabaseKit/SpectraSupabaseKit.swift
@@ -11,19 +11,27 @@ public struct SupabaseClients {
         supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impud2dubWdlZnRicG1nbmh0am5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNjkwMDUsImV4cCI6MjA2Njg0NTAwNX0.QAlOu-jTNU2KOdn_WaGtaGYtW-HvRiW57uM5BHj2Esk",
         options: SupabaseClientOptions(
             db: SupabaseClientOptions.DatabaseOptions.init(
-                encoder: {
-                    let encoder = JSONEncoder()
-                    encoder.dateEncodingStrategy = .iso8601
-                    encoder.keyEncodingStrategy = .convertToSnakeCase
-                    return encoder
-                }(),
-                decoder: {
-                    let decoder = JSONDecoder()
-                    decoder.dateDecodingStrategy = .iso8601
-                    decoder.keyDecodingStrategy = .convertFromSnakeCase
-                    return decoder
-                }()
+                encoder: JSONEncoder.supabaseEncoder,
+                decoder: JSONDecoder.supabaseDecoder
             )
         )
     )
+}
+
+public extension JSONEncoder {
+    static var supabaseEncoder: JSONEncoder {
+        var encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }
+}
+
+public extension JSONDecoder {
+    static var supabaseDecoder: JSONDecoder {
+        var decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
 }
